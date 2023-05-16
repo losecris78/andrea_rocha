ce92e1219e810c6e4fccab92400c24703879a43c
losecris78 <lop22002@byui.edu>
2023-05-16 20:39:21 -0300

    responsive and weather and date

--- js/script.js
+++ js/script.js
@@ -5,20 +5,20 @@ let year = date.getFullYear();
 let dateFormat = new Intl.DateTimeFormat("en-US",{dateStyle:"full"}).format(date);
 let day = date.getDay()
 /*console.log(day);*/
-document.querySelector("today").innerHTML=dateFormat;
-document.querySelector("year").innerHTML= year
+document.querySelector("#today").innerHTML=dateFormat;
+document.querySelector("#year").innerHTML= year
 
 
 //Year display for copyright//
 
-document.querySelector("#year").innerText = year;
+
 
 // select HTML elements in the document
 const currentTemp = document.querySelector('#today_weather');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
-const url ='https://api.openweathermap.org/data/2.5/weather?q=curitiba&units=imperial&appid=082c6f56cd719ed77501c3dc650ee635key';
+const url ='https://api.openweathermap.org/data/2.5/weather?q=curitiba&units=metric&appid=082c6f56cd719ed77501c3dc650ee635';
 
 
 async function apiFetch() {
