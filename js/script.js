33744243517d3887cf35093ce39c4a7f406e0a7b
losecris78 <lop22002@byui.edu>
2023-06-01 19:05:20 -0300

    nav corrections

--- js/script.js
+++ js/script.js
@@ -55,6 +55,7 @@ async function apiFetch() {
 
   function toggleMenu(){
     document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburguerBtn").classList.toggle("open");
   }
   const x= document.getElementById("hamburguerBtn")
   x.onclick= toggleMenu;
